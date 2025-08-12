39f151cf9aec2423fa09812c9c5e3a7d0a4a22f9
baruchInsert-tech <baruchh@insert-tech.com>
2025-08-12 16:35:08 +0300

    feat: fix commit config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Disable the body-max-line-length rule
+    'body-max-line-length': [0, 'always'],
+  },
+};
